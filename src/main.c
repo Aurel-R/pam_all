7369b878e092a5332c85bfc99ff37df173db16db
Aurelien Rausch <aurelien.rausch@inria.fr>
2015-04-01 17:49:50 +0200

    start simple module

--- src/main.c
+++ src/main.c
@@ -1,1 +1,39 @@
+
+#define PAM_SM_ACCOUNT
+#define PAM_SM_AUTH
+#define PAM_SM_PASSWORD
+#define PAM_SM_SESSION
+
 #include <security/pam_modules.h>
+
+
+/* authentication management  */
+PAM_EXTERN
+int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv){
+        pam_display_message("Test message from pam_shamir");
+	return PAM_IGNORE;
+}
+
+PAM_EXTERN
+int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv){
+        return PAM_IGNORE;
+}
+
+/* account management */
+PAM_EXTERN
+int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv){
+	return PAM_IGNORE;
+}
+
+/* session management */
+PAM_EXTERN
+int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv){
+	return PAM_IGNORE;
+}
+
+PAM_EXTERN
+int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv){
+        return PAM_IGNORE;
+}
+
+
