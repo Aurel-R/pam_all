846c9773e763d7ff59972c0d376f253b3250860c
Aurelien Rausch <aurelien.rausch@inria.fr>
2015-04-08 17:29:27 +0200

    user get

--- src/main.c
+++ src/main.c
@@ -1,3 +1,9 @@
+/* todo : 
+*	- get password
+*	- get tty (env)
+*	- get command line 
+*/
+
 
 #define PAM_SM_ACCOUNT
 #define PAM_SM_AUTH
@@ -5,17 +11,54 @@
 #define PAM_SM_SESSION
 
 #include <security/pam_modules.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
 #include <syslog.h>
 #include <stdarg.h>
+#include <errno.h>
+#include <pwd.h>
 
 #define NAME	"pam_shamir.so"
+
 
 void log_message(int level, char *msg, ...);
 
 /* authentication management  */
 PAM_EXTERN
 int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv){
-        log_message(LOG_NOTICE, "Test message from pam_shamir");
+        int retval, i;
+	int debug_mod = 0; /* take LOG_DEBUG if debug option is specified in conf file */ 
+	char **user = NULL;
+	char *password;
+	struct passwd *pwd = malloc(sizeof(struct passwd));
+
+	if(pwd == NULL){
+		log_message(LOG_CRIT, "malloc() %m");
+		return PAM_SYSTEM_ERR;
+	}
+
+	#ifdef DEBUG
+		debug_mod = LOG_DEBUG;
+	#endif
+
+	log_message(LOG_NOTICE, "the module was started");
+
+	for(i=0; i<argc; i++){
+		if(strcmp(argv[i], "debug") == 0)
+			debug_mod = LOG_DEBUG;
+	}
+	
+	log_message(debug_mod, "debug: verbose mod is activated");
+
+	if(pam_get_user(pamh, &user, NULL) != PAM_SUCCESS){
+		log_message(LOG_ERR, "can't determine user name ; %m");
+		return PAM_USER_UNKNOWN;
+	}
+
+	log_message(debug_mod, "debug: user %s", user);
+
 	return PAM_IGNORE;
 }
 
@@ -48,7 +91,10 @@ void log_message(int level, char *msg, ...){
 	
 	va_start(args, msg);
 	openlog(NAME, LOG_PID, LOG_AUTHPRIV);
-	vsyslog(level, msg, args);
+	
+	if(level) /* level=0:false level>0:true */
+		vsyslog(level, msg, args);
+	
 	closelog();
 	va_end(args);
 }
