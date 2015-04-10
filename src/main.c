317a97e7221423827553af5cdc1245fb7b185724
Aurelien Rausch <aurelien.rausch@inria.fr>
2015-04-10 14:36:58 +0200

    get/compare/save password

--- src/main.c
+++ src/main.c
@@ -10,27 +10,51 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
+#include <unistd.h>
 #include <syslog.h>
 #include <stdarg.h>
 #include <errno.h>
 #include <pwd.h>
+#include <crypt.h>
+#include <shadow.h> 
 
 #define NAME	"pam_shamir.so"
 
+const char passwd_prompt[] = "Unix password: ";
 
-void log_message(int level, char *msg, ...);
+void log_message(int level, char *msg, ...)
+{
+	va_list args;
+	
+	va_start(args, msg);
+	openlog(NAME, LOG_PID, LOG_AUTHPRIV);
+	
+	if (level)
+		vsyslog(level, msg, args);
+	
+	closelog();
+	va_end(args);
+}
+
+
+//int get_password(pam_handle_t *pamh, )
+//int get_user(
+
+
 
 /* authentication management  */
 PAM_EXTERN 
 int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
         int retval, i;
-	int debug_mod = 0; /* take LOG_DEBUG if debug option is specified in conf file */ 
-	char **user = NULL;
+	int debug_mod = 0;
+	char *user = NULL;
 	char *p = NULL;
-	struct passwd *pwd = malloc(sizeof(struct passwd));
+	char *crypt_password = NULL;
+	/* struct passwd *pwd = malloc(sizeof(struct passwd)); */
+	struct spwd *pwd = malloc(sizeof(struct spwd));
 
-	if (pwd == NULL) { //call not_null
+	if (pwd == NULL) {
 		log_message(LOG_CRIT, "malloc() %m");
 		return PAM_SYSTEM_ERR;
 	}
@@ -39,30 +63,86 @@ int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **ar
 		debug_mod = LOG_DEBUG;
 	#endif
 
-	log_message(LOG_NOTICE, "the module was started");
 
+	/*
+	 * Parse all arguments. If debug option is found 
+	 * in configuration file, set the verbose mode 
+	 */
 	for (i=0; i<argc; i++) {
 		if (strcmp(argv[i], "debug") == 0)
 			debug_mod = LOG_DEBUG;
 	}
-	
-	log_message(debug_mod, "debug: verbose mod is activated");
 
-	if ((retval = pam_get_user(pamh, &user, NULL)) != PAM_SUCCESS) {
+	log_message(debug_mod, "debug: the module was started");	
+
+	/* get current user */
+
+	if ((retval = pam_get_user(pamh, (const char **)&user, NULL)) != PAM_SUCCESS) {
 		log_message(LOG_ERR, "can not determine user name: %m");
 		return retval;
 	}
 
 	log_message(debug_mod, "debug: user %s", user);
 
-	if( (retval = pam_prompt(pamh, PAM_PROMPT_ECHO_OFF, &p, "%s", "Unix password : ")) != PAM_SUCCESS) {
-		log_message(LOG_ERR, "can not determine the password");
+	/*
+	 * we will have to get the password from the
+	 * user directly
+	 */
+
+	if((retval = pam_prompt(pamh, PAM_PROMPT_ECHO_OFF, &p, "%s", passwd_prompt)) != PAM_SUCCESS) {
+		log_message(LOG_ERR, "can not determine the password: %m");
 		return retval;
 	} 
 
-	/*log_message(debug_mod, "debug: passwd %s", p);*/  /* You should never use this line (or give fake password) */
 
-	free(pwd);
+	if (p == NULL) {
+		log_message(LOG_NOTICE, "the password is empty");
+		return PAM_AUTH_ERR;
+	}
+
+	
+	/* get user password save in /etc/shadow */
+
+	if ((pwd = getspnam(user)) == NULL) {
+		log_message(LOG_ERR, "can not verify the password for user %s: %m", user);
+		return PAM_USER_UNKNOWN;
+	} 
+
+
+	if ((crypt_password = crypt(p, pwd->sp_pwdp)) == NULL) {
+		log_message(LOG_ERR, "can not crypt password for user %s: %m", user);
+		return PAM_AUTH_ERR;
+	}
+
+	/* compare passwords */
+
+	if ((strcmp(crypt_password, pwd->sp_pwdp) != 0)) {
+		log_message(LOG_NOTICE, "incorrect password attempts");
+		return PAM_AUTH_ERR;
+	}
+
+	log_message(LOG_NOTICE, "user %s has been authenticate", user);	
+	log_message(debug_mod, "debug: set password item");
+
+	if ((retval = pam_set_item(pamh, PAM_AUTHTOK, (const void *)p)) != PAM_SUCCESS) {
+		log_message(LOG_ERR, "can not set password item: %m");
+	} 
+
+	/*
+	 * get user fct ; passwd fct ; and juste verify the return and print 'authentication failure' in LOG_NOTICE
+	 * display message for user "sorry 
+	 */
+	
+
+
+	log_message(debug_mod, "end of module");
+
+	/*
+	 * Do not free the struct. Maybe use a specifick function like clean()... 
+	 * I have to look in man 
+	 */
+	
+/*	free(pwd); */
 	return PAM_IGNORE;
 }
 
@@ -87,22 +167,5 @@ PAM_EXTERN
 int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv){
         return PAM_IGNORE;
 }
-
-
-
-void log_message(int level, char *msg, ...)
-{
-	va_list args;
-	
-	va_start(args, msg);
-	openlog(NAME, LOG_PID, LOG_AUTHPRIV);
-	
-	if (level)
-		vsyslog(level, msg, args);
-	
-	closelog();
-	va_end(args);
-}
-
 
 
