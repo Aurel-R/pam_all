73edd322619bec6248a14395c89ac9ee9f0b1f39
Aurelien Rausch <aurelien.rausch@inria.fr>
2015-04-21 09:59:19 +0200

    shared command

--- src/sudo_plugin/shared.c
+++ src/sudo_plugin/shared.c
@@ -16,20 +16,7 @@
 #define FIFO_PATH	"/var/lib/shamir/"
 #define __dso_public __attribute__((__visibility__("default")))
 
-static char *
-_named_fifo(pid_t process) 
-{
-	char *name;
-	size_t n = sizeof(FIFO_PATH) + sizeof(int) + sizeof(char);
-	name = calloc(n, sizeof(char));
-	
-	if (name == NULL)
-		return NULL;
-
-	snprintf(name, n-1, "%s%d", FIFO_PATH, process);
-
-	return name;
-}
+char **command;
 
 static void
 log_message(int level, const char *msg, ...)
@@ -54,35 +41,20 @@ io_open(unsigned int version, sudo_conv_t conversation,
 	int argc, char *const argv[], char *const user_env[],
 	 char *const args[])
 {
-	int retval, i;
-	FILE *fp;
-	char *fifo;
-	
 	log_message(LOG_INFO, "io_open");
+	int i;
 	
-	fifo = _named_fifo(getpid());
-	
-	
-	if ((retval = mkfifo(fifo, 0600))) {
-		log_message(LOG_ERR, "can not create fifo: %m");
-		return false;
-	}
+	command = malloc((argc+1)*sizeof(char *));
 
-	if ((fd = open(fifo, O_WRONLY))) {
-		log_message(LOG_ERR, "can not open fifo: %m");
-		unlink(fifo);
+	if (command == NULL) {
+		log_message(LOG_ERR, "malloc error: %m");
 		return false;
-	}
-
-	for (i=0; i<argc; i++) {
-		if ((write(fd, argv[i], strlen(argv[i]))) == -1) {
-			log_message(LOG_ERR, "can not write in fifo: %m");
-			break;
-		}
 	}	
-	
-	close(fd);
-	unlink(fifo);
+
+	for (i=0; i<argc; i++)
+		command[i] = argv[i];		
+		
+	command[argc] = NULL;
 	
      	return true;
  }
